ad490f5dfdfcda86ea06a3025e44e6222aae9101
Kevabe <89057457+Kevabe@users.noreply.github.com>
2021-10-20 16:25:40 -0600

    Update mapanimation (5).js

--- mapanimation (5).js
+++ mapanimation (5).js
@@ -1,4 +1,3 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
 const busStops = [
   [-71.093729, 42.359244],
   [-71.094915, 42.360175],
@@ -15,10 +14,8 @@ const busStops = [
 ];
 
  
-// TODO: add your own access token
 mapboxgl.accessToken = 'pk.eyJ1IjoidGVzdHVzZXIxMDAwIiwiYSI6ImNraDkzZ2pkMzAzMHoycnBmMXpvZ3UwZnMifQ.jAE4YsPeAJv50VK92NSpOQ';
 
-// This is the map instance
 let map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
@@ -31,10 +28,8 @@ var marker = new mapboxgl.Marker()
 .setLngLat([-71.104081, 42.365554])
 .addTo(map);
 
-// counter here represents the index of the current bus stop
 let counter = 0;
 function move() {
-  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
   setTimeout(() => {
     if (counter >= busStops.length) return;
     marker.setLngLat(busStops[counter]);
